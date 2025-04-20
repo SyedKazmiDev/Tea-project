1f4f328a30973e0414a03c22f491c13bb0207398
SyedKazmiDev <saqlainkazmikiemail@gmail.com>
2025-04-20 20:03:05 +0100

    create header and nav bar

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";
+
+   
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-150px');
+        }
+    });
+    
+ 
+
+    
+})(jQuery);
+
